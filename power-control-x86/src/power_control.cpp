22dea0ccec79c354f56376c3ba8b25b28122e6b5
Mohsen Dolaty <mohsen.dolaty@amd.com>
2022-09-06 14:40:00 -0500

    amd-power-control:Call DIMM SPD Info script
    Call dimm-info.sh from the main() function
    to collect DIMM SPD data
    Move PowerRestorePolicyCheck() to the end of main()
    because of timing issues
    
    Signed-off-by: Mohsen Dolaty <mohsen.dolaty@amd.com>

--- power-control-x86/src/power_control.cpp
+++ power-control-x86/src/power_control.cpp
@@ -1487,6 +1487,8 @@ int main(int argc, char* argv[])
 {
     std::cerr << "Start Chassis power control service...\n";
 
+    if (system("/usr/sbin/dimm-info.sh") != 0)
+        std::cerr << "Error calling dimm-info.sh in Chassis power control service \n";
     power_control::conn =
         std::make_shared<sdbusplus::asio::connection>(power_control::io);
 
@@ -1522,8 +1524,6 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    // Check if we need to start the Power Restore policy
-    power_control::powerRestorePolicyCheck();
     power_control::nmiSourcePropertyMonitor();
 
     std::cerr << "Initializing power state. ";
@@ -1810,6 +1810,9 @@ int main(int argc, char* argv[])
     {
         return -1;
     }
+
+    // Check if we need to start the Power Restore policy
+    power_control::powerRestorePolicyCheck();
 
     power_control::io.run();
 
